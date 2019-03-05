f842ccfdcb3a711a42c52e33bcca2898eb6c9e55
xinkai.wu@tescomm.com.cn <xinkai.wu@tescomm.com.cn>
2019-03-05 18:14:03 +0800

    3

--- service/index.js
+++ service/index.js
@@ -7,12 +7,17 @@ const mongoose = require('mongoose')
   // 必须加冒号
 ;(async () => {
   await connect()
+  //写入数据库
   initSchemas()
   const User = mongoose.model('User')
-  let oneUser = new User({ userName: '5555', password: '5555' })
+  let oneUser = new User({ userName: '6666', password: '77777' })
   oneUser.save().then(() => { 
     console.log('插入成功');
   })
+  
+  //读出已经插入进去的数据
+  let  users = await  User.findOne({}).exec()
+  console.log(users)
 })()
 
 app.use(async (ctx) => {
