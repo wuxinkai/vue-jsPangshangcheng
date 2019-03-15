09a0dc4e7b0ab84725b651a863db1b1932ff1451
xinkai.wu@tescomm.com.cn <xinkai.wu@tescomm.com.cn>
2019-03-15 23:10:07 +0800

    4

--- service/database/init.js
+++ service/database/init.js
@@ -2,50 +2,50 @@ const mongoose = require('mongoose')
 const db = "mongodb://localhost/simle-db2" //simle-db数据库的名字
 
 const glob = require('glob')
-const { resolve } = require('path') 
- 
+const { resolve } = require('path')
+    //读取 schema 文件夹下的文件
 exports.initSchemas = () => {
-  glob.sync(resolve(__dirname,'./schema/','**/*.js')).forEach(require)
+    glob.sync(resolve(__dirname, './schema/', '**/*.js')).forEach(require)
 }
 
 mongoose.Promise = global.Promise
 
 //导出方法
 exports.connect = () => {
-  //连接次数
-  let maxConnectTimes = 0
-  //是否连接成功
-  return new Promise((resolve, reject) => {
-    //连接数据库
-    mongoose.connect(db)
-    //增加数据库连接的事件监听
-    mongoose.connection.on('disconnected', err => {
-      console.log(`**************数据库断开`);
-      //进行重连
-      if (maxConnectTimes < 3) {
-        maxConnectTimes++
+    //连接次数
+    let maxConnectTimes = 0
+        //是否连接成功
+    return new Promise((resolve, reject) => {
+        //连接数据库
         mongoose.connect(db)
-      } else {
-        reject(err)
-        throw new Error('数据库出现问题，程序无法搞定，请人为修理......')
-      }
-    })
-    //数据库出现错误的时候
-    mongoose.connection.on('error', err => {
-      console.log(`**************数据库报错`, err);
-      if (maxConnectTimes < 3) {
-        maxConnectTimes++
-        mongoose.connect(db)
-      } else {
-        reject(err)
-        throw new Error('数据库出现问题，程序无法搞定，请人为修理......')
-      }
+            //增加数据库连接的事件监听
+        mongoose.connection.on('disconnected', err => {
+                console.log(`**************数据库断开`);
+                //进行重连
+                if (maxConnectTimes < 3) {
+                    maxConnectTimes++
+                    mongoose.connect(db)
+                } else {
+                    reject(err)
+                    throw new Error('数据库出现问题，程序无法搞定，请人为修理......')
+                }
+            })
+            //数据库出现错误的时候
+        mongoose.connection.on('error', err => {
+                console.log(`**************数据库报错`, err);
+                if (maxConnectTimes < 3) {
+                    maxConnectTimes++
+                    mongoose.connect(db)
+                } else {
+                    reject(err)
+                    throw new Error('数据库出现问题，程序无法搞定，请人为修理......')
+                }
 
+            })
+            //链接打开的时候
+        mongoose.connection.once('open', () => {
+            console.log('链接打开成功!')
+            resolve()
+        })
     })
-    //链接打开的时候
-    mongoose.connection.once('open', () => {
-      console.log('链接打开成功!')
-      resolve()
-    })
-  })
 }
--- service/database/schema/User.js
+++ service/database/schema/User.js
@@ -2,40 +2,40 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId //获取id
 const bcrypt = require('bcrypt') //加密模块
-let SALT_WORK_FACTOR =10 //加盐
-//创建UserShema
+let SALT_WORK_FACTOR = 10 //加盐
+    //创建UserShema 密码
 const UserShema = new Schema({
-  UserId:ObjectId,
-  userName:{unique:true,type:String},
-  password: String,
-  createAt:{type:Date,default:Date.now()},
-  lastLoginAt:{type:Date,default:Date.now()}
-})
-//加密处理
-UserShema.pre('save', function(next){
-  //let user = this
-  console.log(this)
-  bcrypt.genSalt( SALT_WORK_FACTOR,(err,salt)=>{
-      if(err) return next(err)
-      bcrypt.hash(this.password,salt, (err,hash)=>{
-          if(err) return next(err)
-          this.password = hash
-          next()
-      }) 
-  })
+        UserId: ObjectId,
+        userName: { unique: true, type: String },
+        password: String,
+        createAt: { type: Date, default: Date.now() },
+        lastLoginAt: { type: Date, default: Date.now() }
+    })
+    //加密处理
+UserShema.pre('save', function(next) {
+    //let user = this
+    console.log(this)
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) return next(err)
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) return next(err)
+            this.password = hash
+            next()
+        })
+    })
 })
 
 //密码比对的方法  methods是实例方法 必须用new 才能用
 UserShema.methods = {
-  //_password 数据库密码 , password 当前密码
-  comparePassword:(_password,password)=>{
-      return new Promise((resolve,reject)=>{
-          bcrypt.compare(_password,password,(err,isMatch)=>{
-              if(!err) resolve(isMatch)
-              else reject(err)
-          })
-      })
-  }
+    //_password 数据库密码 , password 当前密码
+    comparePassword: (_password, password) => {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(_password, password, (err, isMatch) => {
+                if (!err) resolve(isMatch)
+                else reject(err)
+            })
+        })
+    }
 }
 
 // 发布模型, User对应的是数据库里的users 这个表名字
