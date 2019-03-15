eb37e83d92cd28a6b8f0a5a3825edf992a78de04
xinkai.wu@tescomm.com.cn <xinkai.wu@tescomm.com.cn>
2019-03-15 23:14:44 +0800

    4

--- service/index.js
+++ service/index.js
@@ -1,8 +1,8 @@
 const Koa = require('koa')
 const app = new Koa()
 const {
-  connect,
-  initSchemas
+    connect,
+    initSchemas
 } = require('./database/init.js')
 const mongoose = require('mongoose')
 
@@ -13,7 +13,7 @@ app.use(bodyParser());
 //解决跨域
 const cors = require('koa2-cors')
 app.use(cors())
-//路由
+    //路由
 const Router = require('koa-router')
 let user = require('./appApi/user.js')
 let goods = require('./appApi/goods.js')
@@ -23,24 +23,24 @@ let category = require('./appApi/category.js')
 let router = new Router();
 router.use('/user', user.routes())
 router.use('/goods', goods.routes())
-router.use('/category',category.routes())
-//加载路由中间件
+router.use('/category', category.routes())
+    //加载路由中间件
 app.use(router.routes())
 app.use(router.allowedMethods())
 
 // 必须加冒号
 ;
-(async () => {
-  await connect()
-  //写入数据库
-  initSchemas()
+(async() => {
+    await connect()
+        //写入数据库
+    initSchemas()
 
 })()
 
-app.use(async (ctx) => {
-  ctx.body = '<h1>hello Koa2</h1>'
+app.use(async(ctx) => {
+    ctx.body = '<h1>hello Koa2</h1>'
 })
 
 app.listen(3000, () => {
-  console.log('[Server] starting at port 3000')
+    console.log('[Server] starting at port 3000')
 })
